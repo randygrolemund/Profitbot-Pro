d0429e347fc313265e63ab67fe9a38a5eda26eae
Randy Grolemund <randygrolemund@Randys-MacBook-Pro.local>
2018-09-13 08:01:22 -0500

    removed extra kill process

--- profit_manager.ps1
+++ profit_manager.ps1
@@ -630,26 +630,7 @@ else {
     Set-Variable -Name "fixed_diff" -Value ""
 }
 
-# Kill worker if already running.
-$worker_running = Get-Process $miner_type -ErrorAction SilentlyContinue
-if ($worker_running) {
-    # Write to the log.
-    if (Test-Path $path\$pc\$pc"_"$(get-date -f yyyy-MM-dd).log) {
-        Write-Output "$TimeNow : $miner_type is already running, attempting to stop." | Out-File  -append $path\$pc\$pc"_"$(get-date -f yyyy-MM-dd).log
-    }
-    Write-Host "$TimeNow : Worker already running, stopping process." -ForegroundColor Red
-    # try gracefully first
-    $worker_running.CloseMainWindow() | out-null
-    # kill after five seconds
-    Write-Host "$TimeNow : Pausing for $stop_worker_delay seconds while worker shuts down." -ForegroundColor Yellow
-    Start-Sleep $stop_worker_delay
-    if (!$worker_running.HasExited) {
-        $worker_running | Stop-Process -Force | out-null
-    }
-}
-Remove-Variable worker_running
-
-# Extra kill process if using festival-miner, stop xmr-stak if it was running previously.
+# Kill process if using festival-miner, stop xmr-stak if it was running previously.
 if($miner_type -eq 'festival-miner'){
 
     $worker_running = Get-Process xmr-stak -ErrorAction SilentlyContinue
@@ -671,7 +652,7 @@ if($miner_type -eq 'festival-miner'){
     Remove-Variable worker_running
 }
 
-# Extra kill process if using xmr-stak, stop festival-miner if it was running previously.
+# Kill process if using xmr-stak, stop festival-miner if it was running previously.
 if($miner_type -eq 'xmr-stak'){
 
     $worker_running = Get-Process festival-miner -ErrorAction SilentlyContinue
