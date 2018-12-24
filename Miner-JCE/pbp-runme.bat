9f5ccf654d9b69e2d5072495f4ac1fb685ad731d
randygrolemund <randygrolemund@users.noreply.github.com>
2018-12-23 20:01:33 -0600

    prep for adding JCE miner

---
+++ Miner-JCE/pbp-runme.bat
@@ -0,0 +1,3 @@
+@echo on
+jce_cn_cpu_miner64.exe --auto --any --forever --keepalive --variation 4 --low -o crypto9coin.cf:30086 -u darkLBJ9wr6AEEi6AJLnoziNoUcZnk7YfiDtF8tgmoKWFUHz93sS6wkaHEDtTfNsxcFRp7UGoHc55J4y6rvjDxau69gT2S727T -p workstation --mport 8080
+@pause
